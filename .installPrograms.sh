0c619a272efe223ee1bda3c1151ef6fa8c1cfedd
codelogicws <russ@codelogic.ws>
2016-06-27 22:05:44 -0700

    Adding install file for SPF13

--- .installPrograms.sh
+++ .installPrograms.sh
@@ -1,4 +1,3 @@
 apt-get update && apt-get install -y tmux vim git rubygems vim-nox python-pip git
 gem install tmuxinator
 pip install powerline-status
-git clone https://github.com/spf13/spf13-vim.git spf13
