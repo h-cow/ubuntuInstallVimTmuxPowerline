aacc587de1ca392e2608dcc24decafc45684b5bb
codelogicws <russ@codelogic.ws>
2016-06-27 21:59:05 -0700

    Added in spf13 download

--- .installPrograms.sh
+++ .installPrograms.sh
@@ -1,3 +1,4 @@
-apt-get update && apt-get install -y tmux vim git rubygems vim-nox python-pip
+apt-get update && apt-get install -y tmux vim git rubygems vim-nox python-pip git
 gem install tmuxinator
 pip install powerline-status
+git clone https://github.com/spf13/spf13-vim.git spf13
